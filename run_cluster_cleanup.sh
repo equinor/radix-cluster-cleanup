33f3a0e76aa20c6e6b58a87723b6ad1f6ac27c4c
Magnus Longva <magnus@longva.email>
2023-03-13 14:06:52 +0100

    added env variables as executable args

--- run_cluster_cleanup.sh
+++ run_cluster_cleanup.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-ls
-/radix-cluster-cleanup list-rrs-for-stop
+/radix-cluster-cleanup list-rrs-for-stop \
+  --period=${PERIOD} \
+  --cleanup-start=${CLEANUP_START} \
+  --cleanup-end=${CLEANUP_END} \
+  --cleanup-days=${CLEANUP_DAYS}
+
+/radix-cluster-cleanup list-rrs-for-deletion \
+  --period=${PERIOD} \
+  --cleanup-start=${CLEANUP_START} \
+  --cleanup-end=${CLEANUP_END} \
+  --cleanup-days=${CLEANUP_DAYS}
