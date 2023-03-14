92f536050478c7c0579d4930f8ab3ed31ff0789b
Magnus Longva <magnus@longva.email>
2023-03-14 12:01:17 +0100

    adressed comments

--- radix-cluster-cleanup/main.go
+++ radix-cluster-cleanup/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"github.com/equinor/radix-cluster-cleanup/cmd"
-
-	// Force loading of needed authentication library
-	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 )
 
 func init() {
